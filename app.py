86882d6ff5837a88a6d8bb8e972beb780a20953d
Arjun Khorana <31749972+akhorana14@users.noreply.github.com>
2022-01-22 17:39:23 -0500

    Fixed imports

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, send_from_directory
 from flask_restful import Api, Resource, reqparse
 from flask_cors import CORS #comment this on deployment
 from api.HelloApiHandler import HelloApiHandler
+import sys
 
 app = Flask(__name__, static_url_path='', static_folder='frontend/build')
 CORS(app) #comment this on deployment
@@ -17,3 +18,11 @@ try:
     api.add_resource(PaperApiHandler, '/paper')
 except Exception as e:
     print(e)
+    exception_type, exception_object, exception_traceback = sys.exc_info()
+    filename = exception_traceback.tb_frame.f_code.co_filename
+    line_number = exception_traceback.tb_lineno
+
+    print("Exception type: ", exception_type)
+    print("File name: ", filename)
+    print("Line number: ", line_number)
+
