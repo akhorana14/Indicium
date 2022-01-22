d541066ed512a49315b7426785f2908069056cde
Abhinav Dusi <dusi.abhinav2002@gmail.com>
2022-01-22 17:05:14 -0500

    Fix

--- src/screens/upload.js
+++ src/screens/upload.js
@@ -8,9 +8,9 @@ function Upload() {
                 <div id="upload-title">
                     Upload research paper
                 </div>
-                <input type="text" id="input-title" className="input" placeholder="Title" /><br></br>
-                <input type="text" id="input-author" className="input" placeholder="Author" /><br></br>
-                <input type="text" id="input-count" className="input" placeholder="Copies To Generate" />
+                <input type="text" id="input-title" className="input-upload" placeholder="Title" /><br></br>
+                <input type="text" id="input-author" className="input-upload" placeholder="Author" /><br></br>
+                <input type="text" id="input-count" className="input-upload" placeholder="Copies To Generate" />
                 <textarea id="paper-text" placeholder="Enter Paper Text"></textarea>
                 <button id="upload-submit">UPLOAD TEXT</button>
             </div>
