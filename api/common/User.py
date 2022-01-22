4db163b0890e1d02d60536f9769013ad71a85ebf
Jacob Zietek <xoqcpn@gmail.com>
2022-01-22 01:43:13 -0500

    Added classes Paper and User

--- api/common/User.py
+++ api/common/User.py
@@ -1,4 +1,18 @@
+class User(object):
 
-class User:
+    def __init__(self, id = None, username = None, papers_owned = None, password = None):
+        self.id = id
+        self.username = username
+        self.papers_owned = papers_owned
+        self.password = password
+        pass
 
-    User():
+    def get_dict(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'papers_owned': self.papers_owned,
+            'password': self.password
+        }
+        pass
+    
