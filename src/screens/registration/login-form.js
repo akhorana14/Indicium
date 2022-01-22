166563dcf93818834ed26ccd22ce690180a29aaa
Akash Melachuri <amelachuri@gmail.com>
2022-01-22 01:46:50 -0500

    React.JS

--- src/screens/registration/login-form.js
+++ src/screens/registration/login-form.js
@@ -1,26 +1,26 @@
 // import logo from './logo.svg';
 import './login-form.css';
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
-class LoginForm extends React.Component {
-    render() {
-        return (
-                <div id="login-form">
-                    <div id="login-title">
-                        Login with email
-                    </div>
-                    <input type="text" id="input-email" className="input" placeholder="Email" />
-                    <input type="password" id="input-password" className="input" placeholder="Password" />
-                    <button id="login-submit" onClick={this.goHome}>LOGIN</button>
-                    <div id="no-account">Don't have an account?</div>
-                    <div id="sign-up" onClick={this.props.toggle}>SIGN UP NOW</div>
-                </div>
-        )
+function LoginForm(props) {
+    let navigate = useNavigate();
+    function goHome() {
+        navigate("/home");
     }
 
-    goHome() {
-        window.location.pathname = '/home';
-    }
+    return (
+        <div id="login-form">
+            <div id="login-title">
+                Log in with email
+            </div>
+            <input type="text" id="input-email" className="input" placeholder="Email" />
+            <input type="password" id="input-password" className="input" placeholder="Password" />
+            <button id="login-submit" onClick={goHome}>LOG IN</button>
+            <div id="no-account">Don't have an account?</div>
+            <div id="sign-up" onClick={props.toggle}>SIGN UP NOW</div>
+        </div>
+    )
 }
 
 export default LoginForm;
--- src/screens/registration/signup-form.js
+++ src/screens/registration/signup-form.js
@@ -4,26 +4,25 @@ import React from 'react'
 
 import { useNavigate } from 'react-router-dom'
 
-class SignupForm extends React.Component {
-    render() {
-        return (
-                <div id="signup-form">
-                    <div id="signup-title">
-                        Sign up with email
-                    </div>
-                    <input type="text" id="input-email" className="input" placeholder="Email" />
-                    <input type="password" id="input-password" className="input" placeholder="Password" />
-                    <input type="password" id="input-password-2" className="input" placeholder="Retype password" />
-                    <button id="signup-submit" onClick={this.goHome}>SIGN UP</button>
-                    <div id="no-account">Already have an account?</div>
-                    <div id="sign-up" onClick={this.props.toggle}>LOG IN NOW</div>
-                </div>
-        )
+function SignupForm(props) {
+    let navigate = useNavigate();
+    function goHome() {
+        navigate("/home");
     }
 
-    goHome() {
-        window.location.pathname = '/home';
-    }
+    return (
+        <div id="signup-form">
+            <div id="signup-title">
+                Sign up with email
+            </div>
+            <input type="text" id="input-email" className="input" placeholder="Email" />
+            <input type="password" id="input-password" className="input" placeholder="Password" />
+            <input type="password" id="input-password-2" className="input" placeholder="Retype password" />
+            <button id="signup-submit" onClick={goHome}>SIGN UP</button>
+            <div id="no-account">Already have an account?</div>
+            <div id="sign-up" onClick={props.toggle}>LOG IN NOW</div>
+        </div>
+    )
 }
 
 export default SignupForm;
