660ec12143c6bc6f1a86ff619377279f7e923d0e
Jacob Zietek <xoqcpn@gmail.com>
2022-01-22 13:31:31 -0500

    khusf

---
+++ api/GET_functions.py
@@ -0,0 +1,21 @@
+from api.common.Paper import Paper
+from common import *
+
+def GetResearchPaperList():
+    # Gets all research paper data from the database
+
+    data = []
+
+    #data = getArrayOfDictsFromSQLQuery
+
+    return data
+
+
+def GetAbstract(id: int):
+    # Gets the abstract of a paper from the database
+
+    return 0
+
+
+
+
