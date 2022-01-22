ed574a89224bdeb52e0d7819d2c9bf2be735ba7b
Abhinav Dusi <dusi.abhinav2002@gmail.com>
2022-01-22 16:56:23 -0500

    Upload

--- src/screens/upload.js
+++ src/screens/upload.js
@@ -4,8 +4,15 @@ import React from 'react'
 
 function Upload() {
         return (
-            <div>
-                
+            <div id="upload-form">
+                <div id="upload-title">
+                    Upload research paper
+                </div>
+                <input type="text" id="input-title" className="input" placeholder="Title" /><br></br>
+                <input type="text" id="input-author" className="input" placeholder="Author" /><br></br>
+                <input type="text" id="input-count" className="input" placeholder="Copies To Generate" />
+                <textarea id="paper-text" placeholder="Enter Paper Text"></textarea>
+                <button id="upload-submit">UPLOAD TEXT</button>
             </div>
         )
 }
